3200a761593c1a3f1edfd4fa1a455dfaa836ab3b
Mike Shecket <mshecket@gmail.com>
2019-11-17 21:09:27 -0500

    Update reverseList.js
    Removed excess comments

--- reverseList.js
+++ reverseList.js
@@ -24,36 +24,21 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    // Initialize a node pointer with the head node
     var currentNode = head;
-    // Check whether the list is empty, and, if
-    // so, return null
+    
     if (head == null)
         return null;
-    // Initialize a node for our output that begins
-    // with a value the same as our first value
-    // and next set to null
+    
     var output = new ListNode(currentNode.val);
-    // Initialize a placeholder to start building
-    // our reversed list
+
     var nextNode = new ListNode();
-    // Iterate until we reach the end of the list
-    // that was passed to us
+
     while (currentNode.next != null)
         {
-            // Advance to the next node in the list
-            // we were given
             currentNode = currentNode.next;
-            // Set our placeholder to hold the value
-            // at the current node in the input list
             nextNode = new ListNode(currentNode.val);
-            // But, cruically, link to the output we
-            // have currently as the next node
             nextNode.next = output;
-            // And refresh our output node with
-            // the placeholder
             output = nextNode;
         }
-    // Return the reversed list we've created
     return output;
 };
